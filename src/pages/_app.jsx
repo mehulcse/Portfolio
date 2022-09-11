8648d1e8358ffcd5b4779819d7d1a00d764f9edb
Mehul Thakkar <mehulthakkar02@gmail.com>
2022-09-11 19:57:08 +0530

    Style enhancements

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
 
-import '@/styles/tailwind.css'
-import 'focus-visible'
+import '@/styles/tailwind.css';
+import 'focus-visible';
 
 function usePrevious(value) {
-  let ref = useRef()
+  let ref = useRef();
 
   useEffect(() => {
-    ref.current = value
-  }, [value])
+    ref.current = value;
+  }, [value]);
 
-  return ref.current
+  return ref.current;
 }
 
 export default function App({ Component, pageProps, router }) {
-  let previousPathname = usePrevious(router.pathname)
+  let previousPathname = usePrevious(router.pathname);
 
   return (
     <>
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+          <div className="w-full bg-white dark:bg-zinc-900 dark:ring-zinc-300/20" />
         </div>
       </div>
       <div className="relative">
@@ -34,5 +34,5 @@ export default function App({ Component, pageProps, router }) {
         <Footer />
       </div>
     </>
-  )
+  );
 }
