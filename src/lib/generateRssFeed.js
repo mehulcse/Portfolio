04c7c9b3c88969297ee5f0364287105067848b84
Mehul Thakkar <mehulthakkar02@gmail.com>
2022-09-11 18:43:09 +0530

    Updated Blog feed

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -14,7 +14,8 @@ export async function generateRssFeed() {
 
   let feed = new Feed({
     title: author.name,
-    description: 'Your blog description',
+    description:
+      'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
     author,
     id: siteUrl,
     link: siteUrl,
